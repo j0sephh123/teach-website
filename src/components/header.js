539a105a7235c013acba8fdf9197cf6c5730cd7f
j0sephh123 <iossif123gg@gmail.com>
2022-02-19 10:41:21 +0200

    simply add header markup

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,41 @@
 import * as React from "react"
 
-const Header = () => <header>header</header>
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <div className="header-desktop">
+          <div className="header-title-nav-wrapper">
+            <div className="header-title">
+              <a>Clove Therapy</a>
+            </div>
+            <div className="header-nav">
+              <div className="header-nav-list">
+                <div className="header-nav-item">
+                  <a href="/about">About</a>
+                </div>
+                <div className="header-nav-item">
+                  <a href="/services">Services</a>
+                </div>
+                <div className="header-nav-item">
+                  <a href="/team">Team</a>
+                </div>
+                <div className="header-nav-item">
+                  <a href="/blog">Blog</a>
+                </div>
+                <div className="header-nav-item">
+                  <a href="/contact">Contact</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="header-actions">
+            <button>Get started</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 export default Header
